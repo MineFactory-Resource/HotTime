b6dc5cdad9e5bacd2cba20d9511644e0a1572a87
all298lie <whdudfo333@naver.com>
2022-01-01 23:12:57 +0900

    Revert "HotTime/feat: HOT-1"
    This reverts commit d6cda050

--- src/main/java/net/teamuni/hottime/HotTime.java
+++ src/main/java/net/teamuni/hottime/HotTime.java
@@ -1,42 +1,17 @@
 package net.teamuni.hottime;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public final class HotTime extends JavaPlugin {
 
-    private static HotTime instance;
-
-    public static Config config;
-
     @Override
     public void onEnable() {
-        instance = this;
+        // Plugin startup logic
 
-        // config.yml 셋업
-        config = new Config();
-
-        // 이벤트 목록을 불러와서 전부 실행
-        for (Task task : config.tasks.values()) {
-            Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-                SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-                String time = format.format(new Date((System.currentTimeMillis())));
-
-                if (time.equals(task.getTime()+":00")) {
-                    task.run();
-                }
-            }, 20, 20);
-        }
     }
 
     @Override
     public void onDisable() {
-        // code
-    }
-
-    public static HotTime getInstance() {
-        return instance;
+        // Plugin shutdown logic
     }
 }
